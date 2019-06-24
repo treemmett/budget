eee4be67420a50f5bc148abd5c18f62bc2dfb24c
treemmett <tregan@tregan.me>
2019-06-23 23:15:29 -0600

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'prettier/react'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  root: true,
+  env: {
+    browser: true,
+    es6: true
+  },
+  rules: {
+    'no-param-reassign': 0,
+    'prettier/prettier': [2, { singleQuote: true }],
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx', '.jsx'] }],
+    '@typescript-eslint/explicit-function-return-type': [
+      2,
+      { allowExpressions: true, allowTypedFunctionExpressions: true }
+    ]
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
+};
