4b0b37e0ae8f90811fbbb6deece37afeddffbed3
Tregan Emmett <tregan@tregan.me>
2020-01-27 16:56:32 -0700

    chore: move user logic to controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,70 @@
 import Config from '../utils/config';
+import HttpException from '../utils/HttpException';
 import Token from '../entities/Token';
 import User from '../entities/User';
+import bcrypt from 'bcryptjs';
 import { getManager } from 'typeorm';
 import jwt from 'jsonwebtoken';
+import uuid from 'uuid/v4';
 
 export default class UserController {
+  public user: User;
+
+  public constructor(user: User) {
+    this.user = user;
+  }
+
+  public static async createUser(
+    email: string,
+    firstName: string,
+    lastName: string,
+    password: string
+  ): Promise<User> {
+    const user = new User();
+    user.email = email;
+    user.firstName = firstName;
+    user.lastName = lastName;
+
+    // hash password through bcrypt
+    const hash = await bcrypt.hash(password, 10);
+    user.hash = Buffer.from(hash);
+
+    await getManager().save(user);
+
+    return user;
+  }
+
+  public static async login(
+    email: string,
+    password: string
+  ): Promise<{ jwt: string; token: Token; user: User }> {
+    const user = await getManager().findOne(User, { where: { email } });
+
+    if (!user) {
+      throw new HttpException({
+        error: 'unauthorized_request',
+        message: 'Email not registered',
+        status: 401
+      });
+    }
+
+    const hashMatch = await bcrypt.compare(password, user.hash.toString());
+
+    if (!hashMatch) {
+      throw new HttpException({
+        error: 'unauthorized_request',
+        message: 'Incorrect password.',
+        status: 401
+      });
+    }
+
+    const { token, jwt: jwtToken } = await new UserController(
+      user
+    ).createToken();
+
+    return { token, jwt: jwtToken, user };
+  }
+
   public static async verifyToken(token?: string): Promise<User | void> {
     try {
       if (!token) {
@@ -30,5 +90,33 @@ export default class UserController {
     } catch (e) {
       //
     }
+  }
+
+  public async createToken(): Promise<{
+    jwt: string;
+    token: Token;
+  }> {
+    const token = new Token();
+    token.jti = uuid();
+    token.expires = new Date(Date.now() + 1000 * 60 * 60);
+    token.user = this.user;
+
+    const signedToken = jwt.sign(
+      {
+        exp: Math.floor(token.expires.getTime() / 1000)
+      },
+      Config.JWT_SECRET,
+      {
+        jwtid: token.jti,
+        subject: this.user.id
+      }
+    );
+
+    await getManager().save(token);
+
+    return {
+      jwt: signedToken,
+      token
+    };
   }
 }
--- src/graphql/UserResolver.ts
+++ src/graphql/UserResolver.ts
@@ -8,17 +8,15 @@ import {
   Resolver,
   Root
 } from 'type-graphql';
-import HttpException from '../utils/HttpException';
+import { IsEmail } from 'class-validator';
 import Token from '../entities/Token';
 import User from '../entities/User';
-import bcrypt from 'bcryptjs';
-import config from '../utils/config';
+import UserController from '../controllers/UserController';
 import { getManager } from 'typeorm';
-import jwt from 'jsonwebtoken';
-import uuid from 'uuid/v4';
 
 @InputType({ description: 'New user data' })
 class CreateUserInput {
+  @IsEmail()
   @Field()
   public email: string;
 
@@ -56,70 +54,20 @@ export default class UserResolver {
   }
 
   @Mutation(() => User)
-  public async createUser(@Arg('data') data: CreateUserInput): Promise<User> {
-    const user = new User();
-    user.email = data.email;
-    user.firstName = data.firstName;
-    user.lastName = data.lastName;
-
-    // hash password through bcrypt
-    const hash = await bcrypt.hash(data.password, 10);
-
-    user.hash = Buffer.from(hash);
-
-    await getManager().save(user);
-
-    return user;
+  public createUser(@Arg('data') data: CreateUserInput): Promise<User> {
+    return UserController.createUser(
+      data.email,
+      data.firstName,
+      data.lastName,
+      data.password
+    );
   }
 
   @Mutation(() => LoginToken)
-  public async login(
+  public login(
     @Arg('email') email: string,
     @Arg('password') password: string
   ): Promise<LoginToken> {
-    const user = await getManager().findOne(User, { where: { email } });
-
-    if (!user) {
-      throw new HttpException({
-        error: 'unauthorized_request',
-        message: 'Email not registered',
-        status: 401
-      });
-    }
-
-    const hashMatch = await bcrypt.compare(password, user.hash.toString());
-
-    if (!hashMatch) {
-      throw new HttpException({
-        error: 'unauthorized_request',
-        message: 'Incorrect password.',
-        status: 401
-      });
-    }
-
-    // generate jwt
-    const token = new Token();
-    token.jti = uuid();
-    token.expires = new Date(Date.now() + 1000 * 60 * 60);
-    token.user = user;
-
-    const signedToken = jwt.sign(
-      {
-        exp: Math.floor(token.expires.getTime() / 1000)
-      },
-      config.JWT_SECRET,
-      {
-        jwtid: token.jti,
-        subject: user.id
-      }
-    );
-
-    await getManager().save(Token, token);
-
-    return {
-      token,
-      user,
-      jwt: signedToken
-    };
+    return UserController.login(email, password);
   }
 }
