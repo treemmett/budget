a80b884a702b0fb9f9f63b16ee11a2071034af4b
Tregan <tregan@tregan.me>
2022-08-27 23:49:18 +0200

    refactor(accounts): remove graphql

--- src/views/Accounts/Accounts.tsx
+++ src/views/Accounts/Accounts.tsx
@@ -1,137 +1,33 @@
-import React, { FC, useState } from 'react';
-import { useMutation, useQuery } from '@apollo/react-hooks';
-import Account from './components/Account';
-import { Account as AccountType } from 'rudget';
-import { BudgetProps } from '../Budget/Budget';
-import Fab from '../../components/Fab/Fab';
-import Loader from '../../components/Loader/Loader';
-import { RouteComponentProps } from '@reach/router';
+import { Account as AccountType } from 'budget';
 import cx from 'classnames';
-import gql from 'graphql-tag';
+import { NextPage } from 'next';
+import React, { useState } from 'react';
+import Fab from '../../components/Fab/Fab';
 import styles from './Accounts.scss';
-import submitInput from '../../utils/submitInput';
+import Account from './components/Account';
 
-interface GetAccounts {
-  budget: {
-    accounts: AccountType[];
-  };
-}
-
-interface GetAccountsInput {
-  budgetId: string;
-}
-
-const GET_ACCOUNTS = gql`
-  query GetAccounts($budgetId: ID!) {
-    budget(id: $budgetId) {
-      accounts {
-        id
-        name
-      }
-    }
-  }
-`;
-
-interface CreateAccount {
-  createAccount: AccountType;
-}
-
-interface CreateAccountInput {
-  name: string;
-  budgetId: string;
-}
-
-const CREATE_ACCOUNT = gql`
-  mutation CreateAccount($name: String!, $budgetId: ID!) {
-    createAccount(name: $name, budgetId: $budgetId) {
-      id
-      name
-    }
-  }
-`;
-
-const Accounts: FC<RouteComponentProps<BudgetProps>> = ({ budgetId }) => {
-  const { data, error, loading } = useQuery<GetAccounts, GetAccountsInput>(
-    GET_ACCOUNTS,
-    { variables: { budgetId } }
-  );
-
+const Accounts: NextPage = () => {
   const [isCreatingAccount, setIsCreatingAccount] = useState(false);
-  const [createAccount] = useMutation<CreateAccount, CreateAccountInput>(
-    CREATE_ACCOUNT,
+  const accounts: AccountType[] = [
     {
-      optimisticResponse(input) {
-        return {
-          createAccount: {
-            id: Math.random().toString(),
-            name: input.name,
-          },
-        };
-      },
-      update(cache, response) {
-        const cached = cache.readQuery<GetAccounts, GetAccountsInput>({
-          query: GET_ACCOUNTS,
-          variables: { budgetId },
-        });
-
-        cached.budget.accounts.push(response.data.createAccount);
-
-        cache.writeQuery({
-          data: cached,
-          query: GET_ACCOUNTS,
-          variables: { budgetId },
-        });
-      },
-    }
-  );
-
-  if (error) {
-    return (
-      <div className={styles.view}>
-        Something went wrong. Please try again later.
-      </div>
-    );
-  }
-
-  if (loading) {
-    return (
-      <div className={styles.viewLoading}>
-        <Loader size="large" />
-      </div>
-    );
-  }
+      id: 'a',
+      name: 'Checking',
+    },
+  ];
 
   return (
     <div className={styles.view}>
       <div className={styles.section}>
         <div className={styles.title}>Bank Accounts</div>
         <div className={styles.grid}>
-          {data.budget.accounts.map(account => (
+          {accounts.map((account) => (
             <div className={styles.cell} key={account.id}>
               <Account id={account.id} name={account.name} />
             </div>
           ))}
           {isCreatingAccount && (
             <div className={cx(styles.card, styles.input)}>
-              <input
-                onBlur={() => setIsCreatingAccount(false)}
-                onKeyDown={e =>
-                  submitInput({
-                    async enter(evt) {
-                      await createAccount({
-                        variables: { budgetId, name: evt.currentTarget.value },
-                      });
-                      setIsCreatingAccount(false);
-                    },
-                    escape() {
-                      setIsCreatingAccount(false);
-                    },
-                    event: e,
-                  })
-                }
-                placeholder="Account Name"
-                autoFocus
-              />
+              <input onBlur={() => setIsCreatingAccount(false)} placeholder="Account Name" />
             </div>
           )}
         </div>
--- src/views/Accounts/components/Account.tsx
+++ src/views/Accounts/components/Account.tsx
@@ -1,48 +1,17 @@
+import cx from 'classnames';
 import React, { FC, useState } from 'react';
 import AccountIcon from '../../../assets/icons/account.svg';
-import { BudgetProps } from '../../Budget/Budget';
 import Edit from '../../../assets/icons/edit.svg';
-import cx from 'classnames';
-import gql from 'graphql-tag';
-import styles from '../Accounts.scss';
 import submitInput from '../../../utils/submitInput';
-import { useMutation } from '@apollo/react-hooks';
-import { useParams } from '@reach/router';
+import styles from '../Accounts.scss';
 
 interface AccountProps {
   id: string;
   name: string;
-  budgetId?: string;
 }
-
-interface RenameAccount {
-  renameAccount: {
-    id: string;
-    name: string;
-  };
-}
-
-interface RenameAccountInput {
-  id: string;
-  name: string;
-  budgetId: string;
-}
-
-const RENAME_ACCOUNT = gql`
-  mutation RenameAccount($id: ID!, $name: String!, $budgetId: ID!) {
-    renameAccount(id: $id, name: $name, budgetId: $budgetId) {
-      id
-      name
-    }
-  }
-`;
 
 const Account: FC<AccountProps> = ({ id, name }) => {
-  const { budgetId } = useParams() as BudgetProps;
   const [isEditing, setIsEditing] = useState(false);
-  const [renameAccount] = useMutation<RenameAccount, RenameAccountInput>(
-    RENAME_ACCOUNT
-  );
 
   return (
     <div className={cx(styles.card, { [styles.isEditing]: isEditing })}>
@@ -51,16 +20,10 @@ const Account: FC<AccountProps> = ({ id, name }) => {
           className={styles.accountName}
           defaultValue={name}
           onBlur={() => setIsEditing(false)}
-          onKeyDown={e =>
+          onKeyDown={(e) =>
             submitInput({
               async enter() {
-                await renameAccount({
-                  variables: {
-                    budgetId,
-                    id,
-                    name: e.currentTarget.value,
-                  },
-                });
+                console.log('changing', id, e.currentTarget.value);
                 setIsEditing(false);
               },
               escape() {
@@ -69,16 +32,11 @@ const Account: FC<AccountProps> = ({ id, name }) => {
               event: e,
             })
           }
-          autoFocus
         />
       ) : (
         <div className={styles.accountName}>{name}</div>
       )}
-      <button
-        className={styles.edit}
-        onClick={() => setIsEditing(true)}
-        type="button"
-      >
+      <button className={styles.edit} onClick={() => setIsEditing(true)} type="button">
         <Edit aria-label="Edit account" />
       </button>
       <AccountIcon className={styles.typeIcon} />
