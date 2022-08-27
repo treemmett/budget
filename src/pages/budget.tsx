09c24eb8970c58fe09fdd0ffc37af8087b432d26
Tregan <tregan@tregan.me>
2022-08-27 22:37:37 +0200

    refactor(budget): resolve type errors

--- src/pages/budget.tsx
+++ src/pages/budget.tsx
@@ -1,78 +1,57 @@
 import { FC } from 'react';
-import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import BudgetGroup from '../components/BudgetGroup';
 import Chevron from '../components/icons/Chevron';
 import styles from './budget.module.scss';
 
-const Budget: FC = () => {
-  function onDragEnd(result: DropResult): void {
-    const { destination, draggableId, source } = result;
-    if (!destination) {
-      return;
-    }
-
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    ) {
-      return;
-    }
-  }
-
-  return (
-    <div className={styles.view}>
-      <div className={styles['page-header']}>
-        <div className={styles['budget-title']}>Budget</div>
-        <div className={styles.date}>
-          <button
-            className={styles.prev}
-            onClick={() => {}}
-          >
-            <Chevron />
-          </button>
-          <div className={styles.text}>
-            {
-              [
-                'January',
-                'February',
-                'March',
-                'April',
-                'May',
-                'June',
-                'July',
-                'August',
-                'September',
-                'October',
-                'November',
-                'December'
-              ][3]
-            }
-            2022
-          </div>
-          <button
-            className={styles.next}
-            onClick={() => {}}
-          >
-            <Chevron />
-          </button>
+const Budget: FC = () => (
+  <div className={styles.view}>
+    <div className={styles['page-header']}>
+      <div className={styles['budget-title']}>Budget</div>
+      <div className={styles.date}>
+        <button className={styles.prev} onClick={() => null}>
+          <Chevron />
+        </button>
+        <div className={styles.text}>
+          {
+            [
+              'January',
+              'February',
+              'March',
+              'April',
+              'May',
+              'June',
+              'July',
+              'August',
+              'September',
+              'October',
+              'November',
+              'December',
+            ][3]
+          }
+          2022
         </div>
+        <button className={styles.next} onClick={() => null}>
+          <Chevron />
+        </button>
       </div>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="budget" type="group">
-          {provided => (
-            <div
-              className={styles['budget-list']}
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-                <BudgetGroup id="g.id" index={0} name="Housing" />
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
     </div>
-  );
-};
+    <DragDropContext onDragEnd={() => null}>
+      <Droppable droppableId="budget" type="group">
+        {(provided) => (
+          <div
+            className={styles['budget-list']}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            <BudgetGroup id="g.id" index={0} name="Housing" />
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  </div>
+);
 
 export default Budget;
