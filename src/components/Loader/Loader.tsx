d0e0980a3032b24d7f599fdf3f58f4cae1e4e970
Tregan <tregan@tregan.me>
2022-08-28 15:04:22 +0200

    fix(loader): use scss modules

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react';
 import cx from 'classnames';
-import styles from './Loader.scss';
+import React, { FC } from 'react';
+import styles from './Loader.module.scss';
 
 interface LoaderProps {
   /** Use secondary blue background color. If false, use primary orange. */
@@ -9,23 +9,21 @@ interface LoaderProps {
   size?: 'small' | 'medium' | 'large';
 }
 
-const Loader: FC<LoaderProps> = ({ secondary, size }) => {
-  return (
-    <div
-      className={cx(styles.loader, {
-        [styles.large]: size === 'large',
-        [styles.medium]: size === 'medium',
-      })}
-    >
-      {new Array(3).fill(null).map((e, i) => (
-        <div
-          className={cx(styles.dot, { [styles.secondary]: secondary })}
-          // eslint-disable-next-line react/no-array-index-key
-          key={i}
-        />
-      ))}
-    </div>
-  );
-};
+const Loader: FC<LoaderProps> = ({ secondary, size }) => (
+  <div
+    className={cx(styles.loader, {
+      [styles.large]: size === 'large',
+      [styles.medium]: size === 'medium',
+    })}
+  >
+    {new Array(3).fill(null).map((e, i) => (
+      <div
+        className={cx(styles.dot, { [styles.secondary]: secondary })}
+        // eslint-disable-next-line react/no-array-index-key
+        key={i}
+      />
+    ))}
+  </div>
+);
 
 export default Loader;
