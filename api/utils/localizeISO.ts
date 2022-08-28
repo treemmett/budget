334238103b2d8ae6a626c67e9b72194082b1a60b
Tregan <tregan@tregan.me>
2022-08-28 11:28:15 +0200

    chore: remove legacy controllers

--- api/utils/localizeISO.ts
+++
@@ -1,7 +0,0 @@
-export default function localizeISO(date: Date): string {
-  const year = date.getFullYear().toString();
-  const month = date.getMonth().toString();
-  const day = date.getDate().toString();
-
-  return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
-}
