60c170eb358ba27033d2e9f30376a2f403473993
Tregan <tregan@tregan.me>
2022-08-28 12:23:51 +0200

    fix(categories): remove graphql

--- src/views/Categories/components/Category.tsx
+++ src/views/Categories/components/Category.tsx
@@ -1,13 +1,7 @@
-import React, { FC, useEffect, useState } from 'react';
-import { toCents, toDisplay } from '../../../utils/formatCurrency';
-import { useMutation, useQuery } from '@apollo/react-hooks';
-import { BudgetProps } from '../../Budget/Budget';
+import React, { FC, useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
-import Loader from '../../../components/Loader/Loader';
-import cx from 'classnames';
-import gql from 'graphql-tag';
-import styles from '../Categories.scss';
-import { useParams } from '@reach/router';
+import { toCents, toDisplay } from '../../../utils/formatCurrency';
+import styles from '../Categories.module.scss';
 
 interface CategoryProps {
   id: string;
@@ -15,159 +9,29 @@ interface CategoryProps {
   groupId: string;
 }
 
-interface GetCategory {
-  budget: {
-    categoryGroup: {
-      category: {
-        allocation: number;
-        name: string;
-      };
-    };
-  };
-}
-
-interface GetCategoryInput {
-  budgetId: string;
-  groupId: string;
-  id: string;
-  date: {
-    month: number;
-    year: number;
-  };
-}
-
-const GET_CATEGORY = gql`
-  query GetCategory(
-    $id: ID!
-    $budgetId: ID!
-    $groupId: ID!
-    $date: AllocationDateInput!
-  ) {
-    budget(id: $budgetId) {
-      categoryGroup(id: $groupId) {
-        id
-        category(id: $id) {
-          allocation(date: $date)
-          name
-        }
-      }
-    }
-  }
-`;
-
-interface AllocateCategory {
-  allocateCategory: {
-    id: string;
-    allocation: number;
-  };
-}
-
-interface AllocateCategoryInput {
-  amount: number;
-  id: string;
-  budgetId: string;
-  date: {
-    month: number;
-    year: number;
-  };
-}
-
-const ALLOCATE_CATEGORY = gql`
-  mutation AllocateCategory(
-    $id: ID!
-    $budgetId: ID!
-    $amount: Int!
-    $date: AllocationDateInput!
-  ) {
-    allocateCategory(
-      id: $id
-      budgetId: $budgetId
-      amount: $amount
-      date: $date
-    ) {
-      id
-      allocation(date: $date)
-    }
-  }
-`;
-
-const Category: FC<CategoryProps> = ({ id, index, groupId }) => {
-  const { budgetId } = useParams() as BudgetProps;
-  const [allocateCategory] = useMutation<
-    AllocateCategory,
-    AllocateCategoryInput
-  >(ALLOCATE_CATEGORY);
+const Category: FC<CategoryProps> = ({ id, index }) => {
   const [allocatedInput, setAllocatedInput] = useState(0);
-  const { data, error, loading } = useQuery<GetCategory, GetCategoryInput>(
-    GET_CATEGORY,
-    {
-      variables: {
-        budgetId,
-        date: {
-          month: new Date().getMonth(),
-          year: new Date().getFullYear(),
-        },
-        groupId,
-        id,
-      },
-    }
-  );
-
-  useEffect(() => {
-    setAllocatedInput(data?.budget.categoryGroup.category.allocation || 0);
-  }, [data]);
 
   function maskInput(input: string): void {
-    // const replacedInput = input.replace(/[^0-9]/g, '');
-    // setAllocatedInput(parseInt(replacedInput, 10));
     setAllocatedInput(toCents(input));
-  }
-
-  if (error) {
-    return (
-      <div className={cx(styles.category, styles.primitive)}>
-        Category failed to load.
-      </div>
-    );
-  }
-
-  if (loading) {
-    return (
-      <div className={cx(styles.category, styles.primitive)}>
-        <Loader />
-      </div>
-    );
   }
 
   return (
     <Draggable draggableId={id} index={index} key={id}>
-      {provided => (
+      {(provided) => (
         <div
           className={styles.category}
           ref={provided.innerRef}
+          // eslint-disable-next-line react/jsx-props-no-spreading
           {...provided.dragHandleProps}
+          // eslint-disable-next-line react/jsx-props-no-spreading
           {...provided.draggableProps}
         >
-          <div className={styles.title}>
-            {data.budget.categoryGroup.category.name}
-          </div>
+          <div className={styles.title}>My Category</div>
           <div className={styles.field}>
             <input
               className={styles.input}
-              onBlur={() => {
-                allocateCategory({
-                  variables: {
-                    amount: allocatedInput,
-                    budgetId,
-                    date: {
-                      month: new Date().getMonth(),
-                      year: new Date().getFullYear(),
-                    },
-                    id,
-                  },
-                }).catch(() => {});
-              }}
-              onChange={e => maskInput(e.currentTarget.value)}
+              onChange={(e) => maskInput(e.currentTarget.value)}
               placeholder="$0.00"
               value={allocatedInput > 0 ? toDisplay(allocatedInput) : ''}
             />
--- src/views/Categories/components/Group.tsx
+++ src/views/Categories/components/Group.tsx
@@ -1,17 +1,12 @@
-import { Budget, TransactionCategory } from 'rudget';
-import { Draggable, Droppable } from 'react-beautiful-dnd';
-import React, { FC, useEffect, useState } from 'react';
-import { useMutation, useQuery } from '@apollo/react-hooks';
-import AnimateHeight from 'react-animate-height';
-import Category from './Category';
-import ChevronDown from '../../../assets/icons/chevronDown.svg';
-import Loader from '../../../components/Loader/Loader';
-import Plus from '../../../assets/icons/plusCircle.svg';
 import cx from 'classnames';
-import gql from 'graphql-tag';
-import styles from '../Categories.scss';
-import submitInput from '../../../utils/submitInput';
+import React, { FC, useState } from 'react';
+import AnimateHeight from 'react-animate-height';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
+import ChevronDown from '../../../assets/icons/chevronDown.svg';
+import Plus from '../../../assets/icons/plusCircle.svg';
 import { toDisplay } from '../../../utils/formatCurrency';
+import styles from '../Categories.module.scss';
+import Category from './Category';
 
 interface GroupProps {
   budgetId: string;
@@ -19,218 +14,33 @@ interface GroupProps {
   index: number;
 }
 
-interface GetCategoryGroup {
-  budget: Budget;
-}
-
-interface GetCategoryGroupInput {
-  budgetId: string;
-  date: {
-    month: number;
-    year: number;
-  };
-  id: string;
-}
-
-const GET_CATEGORY_GROUP = gql`
-  query GetCategoryGroup(
-    $id: ID!
-    $budgetId: ID!
-    $date: AllocationDateInput!
-  ) {
-    budget(id: $budgetId) {
-      categoryGroup(id: $id) {
-        id
-        name
-        categories {
-          id
-          name
-          allocation(date: $date)
-          sort
-        }
-      }
-    }
-  }
-`;
-
-interface CreateCategory {
-  createCategory: TransactionCategory;
-}
-
-interface CreateCategoryInput {
-  budget: string;
-  date: {
-    month: number;
-    year: number;
-  };
-  group: string;
-  name: string;
-}
-
-const CREATE_CATEGORY = gql`
-  mutation CreateCategory(
-    $budget: ID!
-    $group: ID!
-    $name: String!
-    $date: AllocationDateInput!
-  ) {
-    createCategory(budgetId: $budget, groupId: $group, name: $name) {
-      id
-      name
-      allocation(date: $date)
-      sort
-    }
-  }
-`;
-
-interface RenameGroup {
-  renameCategoryGroup: {
-    __typename: string;
-    id: string;
-    name: string;
-  };
-}
-
-interface RenameGroupInput {
-  id: string;
-  name: string;
-  budgetId: string;
-}
-
-const RENAME_GROUP = gql`
-  mutation RenameGroup($id: ID!, $budgetId: ID!, $name: String!) {
-    renameCategoryGroup(id: $id, budgetId: $budgetId, name: $name) {
-      id
-      name
-    }
-  }
-`;
-
-const Group: FC<GroupProps> = ({ budgetId, id, index }) => {
-  const { data, error, loading } = useQuery<
-    GetCategoryGroup,
-    GetCategoryGroupInput
-  >(GET_CATEGORY_GROUP, {
-    variables: {
-      budgetId,
-      date: { month: new Date().getMonth(), year: new Date().getFullYear() },
-      id,
-    },
-  });
-
+const Group: FC<GroupProps> = ({ id, index }) => {
   const [isCreatingCategory, setIsCreatingCategory] = useState(false);
-  const [createCategory] = useMutation<CreateCategory, CreateCategoryInput>(
-    CREATE_CATEGORY,
-    {
-      update: (cache, results) => {
-        const cached = cache.readQuery<GetCategoryGroup, GetCategoryGroupInput>(
-          {
-            query: GET_CATEGORY_GROUP,
-            variables: {
-              budgetId,
-              date: {
-                month: new Date().getMonth(),
-                year: new Date().getFullYear(),
-              },
-              id,
-            },
-          }
-        );
-
-        cached.budget.categoryGroup.categories.push(
-          results.data.createCategory
-        );
-
-        cache.writeQuery<GetCategoryGroup, GetCategoryGroupInput>({
-          data: cached,
-          query: GET_CATEGORY_GROUP,
-          variables: {
-            budgetId,
-            date: {
-              month: new Date().getMonth(),
-              year: new Date().getFullYear(),
-            },
-            id,
-          },
-        });
-      },
-    }
-  );
-
   const [collapsed, setCollapsed] = useState(false);
-
   const [isChangingTitle, setIsChangingTitle] = useState(false);
-  const [renameGroup] = useMutation<RenameGroup, RenameGroupInput>(
-    RENAME_GROUP
-  );
-
-  useEffect(() => {
-    setCollapsed(loading);
-  }, [loading]);
-
-  if (loading) {
-    return (
-      <div className={cx(styles.group, styles.primitive)}>
-        <Loader />
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className={cx(styles.group, styles.primitive)}>
-        Failed to load group {id}.
-      </div>
-    );
-  }
 
   return (
     <Draggable draggableId={id} index={index}>
-      {provided => (
+      {(provided) => (
         <div
           className={cx(styles.group, { [styles.collapsed]: collapsed })}
           ref={provided.innerRef}
+          // eslint-disable-next-line react/jsx-props-no-spreading
           {...provided.draggableProps}
         >
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           <div className={styles.header} {...provided.dragHandleProps}>
             {isChangingTitle ? (
               <input
                 aria-label="Group name"
                 className={cx(styles.title, styles.edit)}
-                defaultValue={data.budget.categoryGroup.name}
+                defaultValue="My Group"
                 onBlur={() => setIsChangingTitle(false)}
-                onKeyDown={e =>
-                  submitInput({
-                    enter: async () => {
-                      await renameGroup({
-                        optimisticResponse: {
-                          renameCategoryGroup: {
-                            __typename: 'CategoryGroup',
-                            id,
-                            name: e.currentTarget.value,
-                          },
-                        },
-                        variables: {
-                          budgetId,
-                          id,
-                          name: e.currentTarget.value,
-                        },
-                      });
-                      setIsChangingTitle(false);
-                    },
-                    escape: () => setIsChangingTitle(false),
-                    event: e,
-                  })
-                }
-                autoFocus
               />
             ) : (
               <div className={styles.title}>
-                <span
-                  className={styles.text}
-                  onDoubleClick={() => setIsChangingTitle(true)}
-                >
-                  {data.budget.categoryGroup.name}
+                <span className={styles.text} onDoubleClick={() => setIsChangingTitle(true)}>
+                  My Group
                 </span>
                 <button
                   className={styles.plus}
@@ -243,77 +53,44 @@ const Group: FC<GroupProps> = ({ budgetId, id, index }) => {
                   <div className={styles.tooltip}>
                     <input
                       onBlur={() => setIsCreatingCategory(false)}
-                      onKeyDown={e =>
-                        submitInput({
-                          enter: async evt => {
-                            await createCategory({
-                              variables: {
-                                budget: budgetId,
-                                date: {
-                                  month: new Date().getMonth(),
-                                  year: new Date().getFullYear(),
-                                },
-                                group: id,
-                                name: evt.currentTarget.value,
-                              },
-                            });
-                            setIsCreatingCategory(false);
-                          },
-                          escape: () => setIsCreatingCategory(false),
-                          event: e,
-                        })
-                      }
                       placeholder="New category name"
-                      autoFocus
                     />
                   </div>
                 )}
               </div>
             )}
             <div className={styles.key}>Allocated</div>
-            <div className={styles.field}>
-              {toDisplay(
-                data.budget.categoryGroup.categories.reduce(
-                  (acc, cur) => acc + cur.allocation,
-                  0
-                )
-              )}
-            </div>
+            <div className={styles.field}>{toDisplay(343343)}</div>
             <div className={styles.border} />
             <button
-              aria-label={`Collapse group ${data.budget.categoryGroup.name}`}
+              aria-label="Collapse group my group"
               className={styles.toggle}
-              onClick={() => setCollapsed(s => !s)}
+              onClick={() => setCollapsed((s) => !s)}
               type="button"
             >
               <ChevronDown />
             </button>
           </div>
           <Droppable droppableId={id} key={id} type="categories">
-            {categoriesProvided => (
-              <AnimateHeight
-                duration={300}
-                easing="ease-in-out"
-                height={collapsed ? 0 : 'auto'}
-              >
+            {(categoriesProvided) => (
+              <AnimateHeight duration={300} easing="ease-in-out" height={collapsed ? 0 : 'auto'}>
                 <div
                   className={styles.categories}
                   ref={categoriesProvided.innerRef}
+                  // eslint-disable-next-line react/jsx-props-no-spreading
                   {...categoriesProvided.droppableProps}
                 >
-                  {[...data.budget.categoryGroup.categories]
+                  {[
+                    { id: 'a', sort: 1 },
+                    { id: 'b', sort: 2 },
+                  ]
                     .sort((a, b) => {
                       if (a.sort > b.sort) return 1;
                       if (a.sort < b.sort) return -1;
                       return 0;
                     })
                     .map((category, i) => (
-                      <Category
-                        groupId={id}
-                        id={category.id}
-                        index={i}
-                        key={category.id}
-                      />
+                      <Category groupId={id} id={category.id} index={i} key={category.id} />
                     ))}
                   {categoriesProvided.placeholder}
                 </div>
